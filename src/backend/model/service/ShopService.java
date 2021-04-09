38a88c30219836cbb7ff1d24f08f07b4186fda29
김효린 <hyorin97@naver.com>
2021-04-09 10:42:27 +0900

    feat: Add Shop Service

---
+++ src/backend/model/service/ShopService.java
@@ -0,0 +1,11 @@
+package backend.model.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import backend.dto.Shop;
+
+public interface ShopService {
+	List<Shop> getShopList(String gu, String dong) throws SQLException;
+	
+}
